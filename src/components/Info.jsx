db3de2bf0b9985f8a6314ee1ec544bb3424750f4
pereruannabaala <pereruannabaala@gmail.com>
2025-08-08 19:37:43 +0300

    push code

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,10 +1,30 @@
+import { CiLinkedin } from "react-icons/ci"
+import { SiGmail } from "react-icons/si"
+import SocialMedia from "./SocialMedia"
+
 export default function Info () {
     return(
         <section>
             <div className="content">
                 <h1>Pereruan Nabaala</h1>
                 <p>Software Engineer</p>
-                <button class="transparent-btn">Linkedin</button>
+                <div className="button">
+                    <button
+                        className="transparent-btn"
+                        type="button"
+                        onClick={() => window.open("https://www.linkedin.com/in/pereruannabaala", "_blank")}
+                    >
+                        <CiLinkedin /> Linkedin
+                    </button>
+
+                    <button type="button"
+                        className="transparent-btn"
+                        onClick={() => window.location.href = "mailto:pereruannabaala@gmail.com"}
+                    >
+                        <SiGmail />Email
+                    </button>
+                </div>
+                <SocialMedia />
             </div> 
 
         </section> 
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,9 @@
+export default function SocialMedia() {
+  return (
+    <div className="socials">
+        <button type="button">GitHub</button>
+        <button type="button">Medium</button>
+        <button type="button">X</button>
+    </div>
+  )
+}

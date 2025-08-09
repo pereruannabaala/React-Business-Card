3aec364872aec486bc733a2f9ade13edc408f043
pereruannabaala <pereruannabaala@gmail.com>
2025-08-09 13:47:05 +0300

    push code

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,9 +1,39 @@
+import { FaGithub, FaMedium, FaInstagram} from "react-icons/fa";
+
 export default function SocialMedia() {
   return (
-    <div className="socials">
-        <button type="button">GitHub</button>
-        <button type="button">Medium</button>
-        <button type="button">X</button>
+    <div className="icon-button">
+
+      <div className="button-container">
+        <button
+          className="round-btn"
+          onClick={() => window.open("https://github.com/pereruannabaala", "_blank")}
+        >
+          <span className="icon"><FaGithub /></span>
+        </button>
+        <div className="label">GitHub</div>
+      </div>
+
+      <div className="button-container">
+        <button
+          className="round-btn"
+          onClick={() => window.open("https://medium.com/@pereruannabaala", "_blank")}
+        >
+          <span className="icon"><FaMedium /></span>
+        </button>
+        <div className="label">Medium</div>
+      </div>
+
+      <div className="button-container">
+        <button 
+          className="round-btn"
+          onClick={() => window.open("https://www.instagram.com/pereruannabaala", "_blank")}
+        >
+          <span className="icon"><FaInstagram /></span>
+        </button>
+        <div className="label">Instagram</div>
+      </div>
     </div>
+
   )
 }
